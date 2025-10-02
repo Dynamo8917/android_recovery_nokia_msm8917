9f9db65c86b5cc6637c094e377e0e0cfdad63096
lavenly <135024283+lavenly@users.noreply.github.com>
2025-10-02 12:40:11 +0000

    E2M: initial device tree

---
+++ README.md
@@ -0,0 +1,21 @@
+Device configuration for Nokia 2.1  (_E2M_)
+=====================================================
+- TeamWin Recovery Project
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Octa-core 1.4 GHz Cortex-A53
+CHIPSET | Qualcomm MSM8917 Snapdragon 425
+GPU     | Adreno 308
+Memory  | 1 GB
+Android Version | 8.1 up to 10 (Go edition)
+Storage | 8 GB
+MicroSD | Up to 128 GB
+Battery | 4000 mAh (non-removable)
+Dimensions | 153.6 × 77.6 × 9.7 mm
+Display | 720 x 1280 pixels, 5.5" IPS
+Rear Camera  | 8.0 MP, f/2.0, LED flash
+Front Camera | 5.0 MP, f/2.4
+Release Date | August 2018
+
+![Nokia 2.1](https://fdn2.gsmarena.com/vv/pics/nokia/nokia-21-1.jpg "Nokia 2.1")
---
+++ omni_E2M.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from E2M device
+$(call inherit-product, device/fih/E2M/device.mk)
+
+PRODUCT_DEVICE := E2M
+PRODUCT_NAME := omni_E2M
+PRODUCT_BRAND := FIH
+PRODUCT_MODEL := Nokia 2.1
+PRODUCT_MANUFACTURER := fih
+
+PRODUCT_GMS_CLIENTID_BASE := android-fih
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="Dynamo2_00WW-user 10 QKQ1.190828.002 00WW_4_11I release-keys"
+
+BUILD_FINGERPRINT := Nokia/Dynamo2_00WW/E2M:10/QKQ1.190828.002/00WW_4_11I:user/release-keys
